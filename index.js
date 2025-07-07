e56a2bd97d5a6f2e414c4cbbee8ad3cb98333d89
Vipulkumar007 <sahuvipulkumar@gmail.com>
2025-07-07 17:36:13 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,16 @@
+require('dot-env')
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.get('/login', (req, res) => {
+    res.send('<h2>Login Page</h2>')
+})
+
+app.listen(process.env.port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
